b325fc28af88e0d217567a1e41ab1e3dc2c72d67
bencher <bencher@users.noreply.github.com>
2018-11-04 19:59:52 +0100

    fix error bij laden zonder file?

--- Game/Memory/Memory/LoadSave.xaml.cs
+++ Game/Memory/Memory/LoadSave.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.IO;
 
 namespace Memory
 {
@@ -19,6 +20,8 @@ namespace Memory
     /// </summary>
     public partial class LoadSave : Window
     {
+        private string delimiter = ";";
+
         public LoadSave()
         {
             InitializeComponent();
@@ -27,18 +30,45 @@ namespace Memory
         private void Save1_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+            string path = @"Save1.csv";
+            if (File.Exists(path))
+            {
+
+            }
+            else
+            {
+                File.WriteAllText(path, delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter);
+            }
             new Spellenscherm1().ShowDialog();
         }
 
         private void Save2_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+            string path = @"Save2.csv";
+            if (File.Exists(path))
+            {
+
+            }
+            else
+            {
+                File.WriteAllText(path, delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter);
+            }
             new Spellenscherm2().ShowDialog();
         }
 
         private void Save3_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+            string path = @"Save2.csv";
+            if (File.Exists(path))
+            {
+
+            }
+            else
+            {
+                File.WriteAllText(path, delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter + Environment.NewLine + delimiter + delimiter + delimiter);
+            }
             new Spellenscherm3().ShowDialog();
         }
     }
